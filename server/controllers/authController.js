f366002eb1cd7051e227aa4ad5038bf7087249b2
Sujan Ban <bansujan@gmail.com>
2025-06-13 21:25:49 +0545

    update cookie settings and change CORS origin for local development

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -37,6 +37,8 @@ const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
+      secure: false,
+      sameSite: "Lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,14 +1,8 @@
 const { register, login, getUser,logout } = require("../controllers/authController");
 const cors = require("cors");
 const { checkAuth } = require("../middlewares/userAuth");
-
 const router = require("express").Router();
-router.use(
-  cors({
-    origin: "https://cms-uuv8.onrender.com",
-    credentials: true,
-  })
-);
+
 router.post("/register", register);
 router.post("/login", login);
 router.get("/logout", logout);
